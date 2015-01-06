6486866cb5f8ec5344c1b80ced67d86a6d9b5568
root <root@localhost.localdomain>
2015-01-06 07:14:47 -0800

    update sol

---
+++ admin/ops/js/sol.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("#datatable").dataTable({
+       "bProcessing":true 
+    });
+});
--- admin/ops/sol/index.php
+++ admin/ops/sol/index.php
@@ -1,6 +1,15 @@
 <?php	
+    if(!isset($_COOKIE['userid']))
+    {
+        header("Location:../../index.php");
+        exit();
+    }
+    $uid=intval($_COOKIE['userid']);
+    $uname=$_COOKIE['username'];
 	include ("../../db/DB.Class.php");
 	include ("../db/func.php"); 
+    $db=new DB();
+    echo "Username:".$uname." ".$uid;
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +18,9 @@
 		<meta charset="utf-8"/>
 		<title></title>
 		<?php getAllStyle(); ?>
+        <script src="../js/sol.js"></script>
 	</head>
 	<body>
-        <div> Hello World</div>
 		<div class="container-fluid">
 			<div id="toolbar" class="row">
 				<nav class="navbar navbar-default" role="navigation">
@@ -26,12 +35,38 @@
 			<div id="lists" class="row metro" style="with:100%;">
 				<table class="table striped hovered dataTable" id="datatable">
 					<thead>
-
+                          <tr>
+                              <th>用户名</th>
+                              <th>题目编号</th>
+                              <th>题目名称</th>
+                              <th>结果</th>
+                              <th>内存</th>
+                              <th>时间</th>
+                              <th>语言</th>
+                              <th>代码长度</th>
+                              <th>提交时间</th>
+                          </tr>
 					</thead>
 					<tbody>
 						<?php
-							$sql="select * from solution";
-						?>
+							$sql="select u.username,p.pid,p.pname,s.result,s.memory,s.time,s.language,s.length,s.create_time from user AS u,problem AS p,solution AS s where u.uid=s.uid AND p.pid=s.pid";
+                            $res=$db->dql($sql);
+                            if($res&&($res->num_rows>0))
+                            {
+                                while($row=$res->fetch_assoc())
+                                {
+                                    echo "<tr><td>".$row['username']."</td>";
+                                    echo "<td>".$row['pid']."</td>";
+                                    echo "<td>".$row['pname']."</td>";
+                                    echo "<td>".$row['result']."</td>";
+                                    echo "<td>".$row['memory']."</td>";
+                                    echo "<td>".$row['time']."</td>";
+                                    echo "<td>".$row['language']."</td>";
+                                    echo "<td>".$row['length']."</td>";
+                                    echo "<td>".$row['create_time']."</td>";
+                                }
+                            }
+						?> 
 					</tbody>
 				</table>
 			</div>
